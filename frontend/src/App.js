2a2d0ba94f5b68472de8c6d0bb64f38d2a5ffd35
Farrah <farrah.unger24@gmail.com>
2023-11-07 21:52:11 -0500

    created frontend react app with axios calls

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Todolist from './components/ToDoList';
+import AddTask from './components/AddTask';
+
+
+function App() {
+  return (
+    <div className="App">
+     <Todolist />
+    </div>
+  );
+}
+
+export default App;
